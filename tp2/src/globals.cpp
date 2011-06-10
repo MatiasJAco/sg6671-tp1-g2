b742ee750ef41dbef8a81f63b50f252b9af24f9a
skirhir@gmail.com <skirhir@gmail.com@8c9cdbba-f223-9568-bec3-c68b3cb6d8cf>
2011-06-10 02:51:50 +0000

    grafica lista de objetos

--- tp2/src/globals.cpp
+++ tp2/src/globals.cpp
@@ -11,3 +11,9 @@ GLuint dl_curvaBezier;
 //Texturas
 GLuint texName;
 GLubyte checkImage[checkImageHeight][checkImageWidth][4];
+
+//Animacion
+int rotation;
+bool animation;
+float currentPosition;
+int target;
--- tp2/src/spline_test/spline.h
+++ tp2/src/spline_test/spline.h
@@ -4,3 +4,6 @@ using namespace std;
 vector<float> calcularPuntosSpline(vector<float> & controlPoints, float step=0.3);
 
 vector<float> calcularPuntosSplineEquiespaciados2D(vector<float> & controlPoints, float paso, float tolerancia=0.02);
+
+vector<float> splinePoint2D(float u, const vector<float> & controlPoints);
+vector<float> splineNormal2D(float u, const vector<float> & controlPoints);
